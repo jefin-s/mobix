19b4fdd853e7341817bcd1484815d4a80fe033b6
jefin-s <jefin.s786@gmail.com>
2025-12-11 20:54:09 +0530

    make progressive web app

--- gentvibe/vite.config.js
+++ gentvibe/vite.config.js
@@ -1,8 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss(), VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // enables PWA during `npm run dev`
+      },
+      manifest: {
+        name: "Icloud",
+        short_name: "Icloud",
+        description: "Apple products selling platform ",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [  
+          {
+            src: "/public/icons/appstore.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pubic/icons/playstore.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
+});
